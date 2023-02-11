7df74a6f80f17570c1a2d9d61ef35bbc46fb3e86
かずのこ <k42un0k0.put1put1@gmail.com>
2023-02-12 06:03:32 +0900

    add storybook

---
+++ src/web/pages/components/Input.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory } from "@storybook/react";
+import { ComponentProps } from "react";
+import Input from "./Input";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Input",
+  component: Input,
+};
+
+const Template = (props: ComponentProps<typeof Input>) => <Input {...props} />;
+
+export const Primary: ComponentStory<typeof Input> = Template.bind({});
